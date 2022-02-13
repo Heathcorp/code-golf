6dc1c0d864c577edcf8b284743b10dc7f6a84167
Heathcorp <heath@manningf.com>
2022-02-13 21:38:03 +1100

    fizzbuzz c 127 bytes

--- Fizz Buzz/fizzbuzz.c
+++ Fizz Buzz/fizzbuzz.c
@@ -1,6 +1,2 @@
 #include<stdio.h>
-int main(){
-	for(int a,b,i=0;i++<100;printf(a&&b?"%s%s%d\n":"%s%s\n",a?"":"Fizz", b?"":"Buzz",i)){
-		a=i%3;b=i%5;
-	}
-}
+int main(){for(int a,b,i=0;i++<100;printf(a*b?"%s%s%d\n":"%s%s\n",a?"":"Fizz",b?"":"Buzz",i)){a=i%3;b=i%5;}}
