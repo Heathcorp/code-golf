747db5704567beef29bbb8ec21a27c6c3c36c447
Heathcorp <heath@manningf.com>
2022-02-13 23:36:28 +1100

    Managed to reduce to 97 bytes

--- Fizz Buzz/fizzbuzz.c
+++ Fizz Buzz/fizzbuzz.c
@@ -1,2 +1,1 @@
-#include<stdio.h>
-int main(){for(int a,b,i=0;i++<100;printf(a*b?"%s%s%d\n":"%s%s\n",a?"":"Fizz",b?"":"Buzz",i)){a=i%3;b=i%5;}}
+main(i,a,b){for(;i<101;a=i%3,b=i%5,printf(a*b?"%s%s%d\n":"%s%s\n",a?"":"Fizz",b?"":"Buzz",i++));}
