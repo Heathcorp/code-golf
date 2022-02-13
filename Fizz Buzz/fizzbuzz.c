aa656b830c04eb1561811ffa3afb34597603d24c
Heathcorp <heath@manningf.com>
2022-02-13 20:52:15 +1100

    Made a shorter c fizzbuzz

--- Fizz Buzz/fizzbuzz.c
+++ Fizz Buzz/fizzbuzz.c
@@ -1,9 +1,6 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
-	int a,b;
-	for(int i=0;i++<100;puts("")){
-		if((a=i%3)==0)printf("Fizz");
-		if((b=i%5)==0)printf("Buzz");
-		if(a&&b)printf("%d",i);
+	for(int a,b,i=0;i++<100;printf(a&&b?"%s%s%d\n":"%s%s\n",a==0?"Fizz":"",b==0?"Buzz":"",i)){
+		a=i%3;b=i%5;
 	}
 }
