f49149f70373bbf6f44be2e3b0ea74d5d1c318ff
Heathcorp <heath@manningf.com>
2022-02-18 01:22:13 +1100

    Did stack pointer magic to reduce diamonds.c by 7

--- Diamonds/diamonds.c
+++ Diamonds/diamonds.c
@@ -1,2 +1,2 @@
-main(j,i,e){for(char*c="12345678987654321";9/j;j++,puts(""))for(i=e=0;i<2*j-1;++i<j?e++:e--)printf("%9.*s%s
-",e,c,c+16-e);}
+main(j,i,e){;for(;9/j;j++,puts(""))for(i=e=0;i<2*j-1;++i<j?e++:e--)printf("%9.*s%s
+",e,"12345678987654321",""-2-e);}
