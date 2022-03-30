0bd25ad8e024cda5cce5497d63c365b0fdaa9eec
Heathcorp <heath@manningf.com>
2022-03-30 17:21:34 +1100

    Reduce asciitable.c to 147 bytes

--- ASCII Table/asciitable.c
+++ ASCII Table/asciitable.c
@@ -1,2 +1,2 @@
 i;main(j){puts("   2 3 4 5 6 7
- -------------");for(;i<16;puts(" DEL"+4*(i<15)),i++){printf("%X:",i);for(j=2;j<8-(i>14);j++){printf(" %c",i+(j<<4));}}}
+ -------------");for(;i<16;puts(" DEL"+4*(i++<15))){printf("%X:",i);for(j=2;j<8-(i>14);j++)printf(" %c",i+(j<<4));}}
