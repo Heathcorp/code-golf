fcc67e37ad755af46401922b307c589496ee1823
Heathcorp <heath@manningf.com>
2022-04-02 17:46:44 +1100

    Reduce rpn.js to 105 bytes

--- Reverse Polish Notation/rpn.js
+++ Reverse Polish Notation/rpn.js
@@ -1,3 +1,1 @@
-for(k of arguments){l=[]
-n=0
-for(a of k.split(' ')){a<'0'?eval(`l[n-2]${a}=l[--n]`):l[n++]=~~a}print(l[0])}
+for(k of arguments)l=[],n=0,k.split(' ').map((a)=>a<'0'?eval(`l[n-2]${a}=l[--n]`):l[n++]=~~a),print(l[0])
