dfdfcbd1194298cc391019242fd1ad29e3ff9986
Heathcorp <heath@manningf.com>
2022-04-10 11:18:33 +1000

    Reduce pernicious.c to 78 bytes

--- Pernicious Numbers/pernicious.c
+++ Pernicious Numbers/pernicious.c
@@ -1,10 +1,2 @@
-main(i,j,n) {
-for(;i<51;i++)
-{
-for(n=0,j=i;j;j/=2)
-{
-n+=j&1;
-}
-n>1&&n^4?printf("%d\n",i):0;
-}
-}
+main(i,j,n){for(;i<51;n>1&&n^4?printf("%d
+",i):0)for(n=0,j=++i;j;j/=2)n+=j&1;}
