5766908e7d7ef65d15e92aebf58d25971585e588
Heathcorp <heath@manningf.com>
2022-04-11 23:24:06 +1000

    Reduce intersection.c to 183 bytes

--- Intersection/intersection.c
+++ Intersection/intersection.c
@@ -1,2 +1,2 @@
-a,b,c,d,e,f,g,h,x,y,z;main(u,v)char**v;{for(;*++v;printf("%d
+b,c,d,e,f,g,h,x,y,z;main(a,v)char**v;{for(;*++v;printf("%d
 ",z))for(sscanf(*v,"%d%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g,&h),z=0,x=a;x<a+c;x++)for(y=b;y<b+d;z+=x>=e&x<e+g&y>=f&y++<f+h);}
