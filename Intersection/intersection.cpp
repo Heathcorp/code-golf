f75745af414162774a2a8f0342b9a9bded153dc9
Heathcorp <heath@manningf.com>
2022-04-11 23:23:03 +1000

    Transfer intersection.c to c++ in 214 bytes

---
+++ Intersection/intersection.cpp
@@ -0,0 +1,2 @@
+#include<stdio.h>
+int b,c,d,e,f,g,h,x,y,z;int main(int a,char**v){for(;*++v;printf("%d\n",z))for(sscanf(*v,"%d%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g,&h),z=0,x=a;x<a+c;x++)for(y=b;y<b+d;z+=x>=e&x<e+g&y>=f&y<f+h,y++);}
