584681da7748fc7ae2faa8ce724a47447a155533
Heathcorp <heath@manningf.com>
2022-05-02 22:44:51 +1000

    Reduce pascals.c to 95 bytes

--- Pascal's Triangle/pascals.c
+++ Pascal's Triangle/pascals.c
@@ -1,1 +1,1 @@
-a[22];b[22];main(j,i){for(a[1]=b[1]=1;j++<21;puts(""),memcpy(a,b,88))for(i=1;i<21;b[i]+=a[i++-1])b[i]?printf("%d ",b[i]):0;}
+b[22];main(j,i){for(b[1]=1;j<21;puts(""))for(i=++j;i;b[i]+=b[i---1])b[i]?printf("%d ",b[i]):0;}
