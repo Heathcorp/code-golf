f45980a90179871501911e854cac0f1f7d843879
Heathcorp <heath@manningf.com>
2022-05-03 09:31:11 +1000

    Reduce pascals.py to 96 bytes

--- Pascal's Triangle/pascals.py
+++ Pascal's Triangle/pascals.py
@@ -1,4 +1,5 @@
-l=[0]*99
-l[1]=1
-for n in range(21):
- for j in range(n):i=n-j;l[i]+=l[i-1];print(l[i],end='\n '[i>1])
+l=[1]
+for n in range(20):
+ l+=[0]
+ for j in range(n):i=n-j;l[i]+=l[i-1]
+ print(*l[:n+1],sep=' ')
