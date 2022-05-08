f706ca1c0f7ca3e56cd1477be1799eb799842764
Heathcorp <heath@manningf.com>
2022-05-09 07:14:18 +1000

    Reduce asciitable.py to 133 bytes

--- ASCII Table/asciitable.py
+++ ASCII Table/asciitable.py
@@ -1,2 +1,2 @@
 print("   2 3 4 5 6 7\n "+13*"-")
-for i in range(16):print("%X:"%i,end='');r=2;exec(6*"print(' '+[chr(r*16+i),'DEL'][r*i>104],end=['','\\n'][r>6]);r+=1;")
+for i in range(16):print("%X: "%i,end='');print(*[[chr(r*16+i),'DEL'][r*i>104]for r in range(2,8)])
