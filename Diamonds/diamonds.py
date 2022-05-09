70c4638e4777ab34a986a433a24133350b4d1814
Heathcorp <heath@manningf.com>
2022-05-10 09:45:08 +1000

    Reduce diamonds.py to 102 bytes

--- Diamonds/diamonds.py
+++ Diamonds/diamonds.py
@@ -1,3 +1,3 @@
-for i in range(2,11):
- for r in [*range(2,i),*range(i,0,-1)]:
-  print((11-r)*' ',*range(1,r),*range(r-2,0,-1),sep='')
+z=range
+for i in z(2,11):
+ for r in [*z(2,i),*z(i,0,-1)]:print((11-r)*' ',*z(1,r),*z(r-2,0,-1),sep='')
