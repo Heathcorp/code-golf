bc9d9ebe89423e1fb83994cace7c4e03425d60f4
Heathcorp <heath@manningf.com>
2022-06-13 23:07:28 +1000

    Reduce trees.js to 97 bytes

--- Christmas Trees/trees.js
+++ Christmas Trees/trees.js
@@ -1,1 +1,1 @@
-for(i=3;i++<10;){for(a=1;a<=i;b=a^i?a:1,print(' '.repeat(i-b-1),'*'.repeat(2*b-1),a++^i?'':'\n'));}
+for(i=3;i++<10;)for(a=1;a<=i;b=a^i?a:1,print(' '.repeat(i-b-1),'*'.repeat(2*b-1),a++^i?'':'\n'));
