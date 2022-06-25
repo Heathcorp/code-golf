92ace12872245ef3bed8275121293337abf6b866
Heathcorp <heath@manningf.com>
2022-06-26 02:50:21 +1000

    Reduce foofizzbuzzbar.py to 138 bytes

--- FooFizzBuzzBar/foofizzbuzzbar.py
+++ FooFizzBuzzBar/foofizzbuzzbar.py
@@ -1,8 +1,8 @@
 for i in range(1,1001):
  s=''
- if i%2==0:s='Foo'
- if i%3==0:s+='Fizz'
- if i%5==0:s+='Buzz'
- if i%7==0:s+='Bar'
+ if i%2<1:s='Foo'
+ if i%3<1:s+='Fizz'
+ if i%5<1:s+='Buzz'
+ if i%7<1:s+='Bar'
  if len(s)<1:s=str(i)
  print(s)
