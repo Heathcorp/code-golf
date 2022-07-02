aa3b95d941a0a3cb2e8410c98a4d75813dd0e86c
Heathcorp <heath@manningf.com>
2022-07-03 00:32:42 +1000

    Reduce foofizzbuzzbar.py to 109 bytes

--- FooFizzBuzzBar/foofizzbuzzbar.py
+++ FooFizzBuzzBar/foofizzbuzzbar.py
@@ -1,6 +1,3 @@
 for i in range(1,1001):
- s=['','Foo'][i%2<1]
- s+='Fizz'[i%3*9:]
- s+='Buzz'[i%5*9:]
- s+='Bar'[i%7*9:]
+ s=['','Foo'][i%2<1]+'Fizz'[i%3*9:]+'Buzz'[i%5*9:]+'Bar'[i%7*9:]
  print([s,i][not s])
