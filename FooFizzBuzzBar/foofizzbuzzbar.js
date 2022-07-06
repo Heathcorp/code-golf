ac49971daf2f9cb46bd7dc8d8de26ecd4e29a646
Heathcorp <heath@manningf.com>
2022-07-07 08:07:49 +1000

    Reduce foofizzbuzzbar.js to 96 bytes

--- FooFizzBuzzBar/foofizzbuzzbar.js
+++ FooFizzBuzzBar/foofizzbuzzbar.js
@@ -1,7 +1,1 @@
-for(i=0;i++<1000;){
-s=i%2?'':'Foo'
-s+=i%3?'':'Fizz'
-s+=i%5?'':'Buzz'
-s+=i%7?'':'Bar'
-print(s?s:i)
-}
+for(i=0;i++<1000;){s=(i%2?'':'Foo')+(i%3?'':'Fizz')+(i%5?'':'Buzz')+(i%7?'':'Bar');print(s?s:i)}
