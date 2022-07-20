62c8bc6ab0f5aa594264705492aae22c170887f7
Heathcorp <heath@manningf.com>
2022-07-20 10:35:31 +1000

    Reduce foofizzbuzzbar.py to 102 bytes

--- FooFizzBuzzBar/foofizzbuzzbar.py
+++ FooFizzBuzzBar/foofizzbuzzbar.py
@@ -1,1 +1,1 @@
-for i in range(1,1001):s=['Foo',''][i%2]+'Fizz'[i%3*9:]+'Buzz'[i%5*9:]+'Bar'[i%7*9:];print([s,i][not s])
+for i in range(1,1001):s='Foo'[i%2*9:]+'Fizz'[i%3*9:]+'Buzz'[i%5*9:]+'Bar'[i%7*9:];print([s,i][not s])
