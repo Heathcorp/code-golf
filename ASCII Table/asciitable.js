350b9a05c0a8045dbbf863c3f5f82f2b88f5cb9a
Heathcorp <heath@manningf.com>
2022-08-01 07:58:11 +1000

    Reduce asciitable.js to 166 bytes

--- ASCII Table/asciitable.js
+++ ASCII Table/asciitable.js
@@ -1,7 +1,3 @@
 write("   2 3 4 5 6 7\n -------------")
-for(i=0;i<16;i++){
-write("\n"+"0123456789ABCDEF"[i]+":")
-for(j=2;j<8;j++){
-write(" "+(i*j<105?String.fromCharCode((j<<4)+i):"DEL"))
-}
-}
+for(i=0;i<16;i++){write("\n"+"0123456789ABCDEF"[i]+":")
+for(j=2;j<8;j++){write("",i*j<105?String.fromCharCode(j*16+i):"DEL")}}
