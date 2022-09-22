e9915d5344d17e02d09c32180870bb31461dbbd9
Heathcorp <heath@manningf.com>
2022-09-23 06:25:25 +1000

    Reduce brainfuck.py to 409 bytes

--- Brainfuck/brainfuck.py
+++ Brainfuck/brainfuck.py
@@ -1,24 +1,23 @@
 import sys
 for arg in sys.argv[1:]:
  i=0
- tape=[0]*999
- p=100
- loops=[-1]
+ tape=[0]*99
+ p=0
+ loops=[]
  skip=-1
  while i < len(arg):
   c=arg[i]
   if c==']':
    ls=loops.pop()
    if skip==ls:skip=-1
-   elif skip==-1 and tape[p]!=0:i=ls-1
+   elif skip<0and tape[p]!=0:i=ls-1
   if c=='[':
    loops.append(i)
-   if skip==-1 and tape[p]==0:skip=i
-  if skip==-1:
+   if skip<0and tape[p]==0:skip=i
+  if skip<0:
    if c=='<':p-=1
    if c=='>':p+=1
    if c=='+':tape[p]+=1
    if c=='-':tape[p]-=1
    if c=='.':print(chr(tape[p]),end='')
-   tape[p]=tape[p]%256
   i+=1
