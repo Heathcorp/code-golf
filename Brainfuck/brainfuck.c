feb89b3d3e92c5697719a1dc86cb58265c7341b2
Heathcorp <heath@manningf.com>
2022-09-28 09:32:36 +1000

    Reduce brainfuck.c to 374 bytes

--- Brainfuck/brainfuck.c
+++ Brainfuck/brainfuck.c
@@ -1,38 +1,22 @@
-main(int argv, char**argc){
-for(;*++argc;){
-// execute each program
-char* prog = *argc;
-int i = 0;
-int p = 0;
-char tape[999] = {0};
-int loops[99] = {0};
-int li = 0;
-int skip = -1;
+int i,li,j;
+main(p,a)char**a;{
+for(;*++a;p=i=li=j=0){
+char tape[999]={0};
+int loops[99]={0};
 char c;
-for(;c=prog[i];i++){
-if(skip<0){
-// execute each brainfuck op
-if(c=='+')tape[p]++;
-if(c=='-')tape[p]--;
-if(c=='>')p++;
-if(c=='<')p--;
-if(c=='.')putchar(tape[p]);
-}
-// loop logic
+for(;c=*(*a+i);i++){
+if(!j){if(c=='+')tape[p]++;if(c=='-')tape[p]--;if(c=='>')p++;if(c=='<')p--;if(c=='.')putchar(tape[p]);}
 if(c=='['){
- loops[li++]=i;
- if(tape[p]==0 && skip<0){
-  skip=i;
- }
+loops[li++]=i;
+if(tape[p]==0 && !j){
+j=i;
+}
 }
 if(c==']'){
- int ls=loops[--li];
- if(skip==ls){
-  skip=-1;
- }else if(skip<0 && tape[p]!=0){
-  i=ls-1;
- }
+int ls=loops[--li];
+if(j==ls){
+j=0;
+}else if(!j && tape[p]!=0){
+i=ls-1;
 }
-}
-}
-}
+}}}}
