80fa909f3658314787af7cba93c5f9610fe8ca34
Heathcorp <heath@manningf.com>
2022-10-06 21:58:10 +1100

    Reduce brainfuck.py to 328 bytes

--- Brainfuck/brainfuck.py
+++ Brainfuck/brainfuck.py
@@ -1,16 +1,16 @@
 import sys
 for a in sys.argv[1:]:
- i=0;t=[0]*99;p=0;l=[];j=-1
+ i=0;t=[0]*99;p=0;l=[];j=0
  while i<len(a):
   c=a[i]
   if c==']':
    k=l.pop()
-   if j==k:j=-1
-   elif j<0and t[p]:i=k-1
+   if j==k:j=0
+   elif j<1and t[p]:i=k-1
   if c=='[':
    l+=[i]
-   if j<0and t[p]<1:j=i
-  if j>0:c=0
+   if j<1and t[p]<1:j=i
+  if j:c=0
   if c=='<':p-=1
   if c=='>':p+=1
   if c=='+':t[p]+=1
