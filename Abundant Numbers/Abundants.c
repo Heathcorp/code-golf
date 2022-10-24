e0535ee9dc11941d0ec9dd6026436517d27b02c9
Heathcorp <heath@manningf.com>
2022-10-25 10:28:52 +1100

    Reduce abundants.c to 76 bytes

--- Abundant Numbers/Abundants.c
+++
@@ -1,5 +0,0 @@
-main(n,i,t){
-for(;n++<200;t>n?printf("%d\n",n):0){
-for(t=0,i=1;i<n;i++)t+=n%i?0:i;
-}
-}
---
+++ Abundant Numbers/abundants.c
@@ -0,0 +1,2 @@
+main(n,i,t){for(;n++<200;t>n?printf("%d
+",n):0)for(t=i=0;++i<n;t+=n%i?0:i);}
