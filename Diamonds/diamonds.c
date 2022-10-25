328faec695164e803578bb2fd196e8276de6d7cd
Heathcorp <heath@manningf.com>
2022-10-26 09:56:21 +1100

    Reduce diamonds.c to 113 bytes

--- Diamonds/diamonds.c
+++ Diamonds/diamonds.c
@@ -1,2 +1,2 @@
-main(j,i,e){;for(;9/j;j++,puts(""))for(i=e=0;i<2*j-1;++i<j?e++:e--)printf("%9.*s%s
+j;main(i,e){for(;j++<9;puts(""))for(i=e=0;i<2*j-1;e+=++i<j?1:-1)printf("%9.*s%s
 ",e,"12345678987654321",""-2-e);}
