23630a454fe9c9b640af979caa8da0755eae576f
Heathcorp <heath@manningf.com>
2022-10-30 10:11:50 +1100

    Reduce sevensegment.py to 243 bytes

--- Seven Segment/sevensegment.py
+++ Seven Segment/sevensegment.py
@@ -1,5 +1,5 @@
 import sys
 s=sys.argv[1]
 print(*[[' _','  '][c in'14']for c in s])
-print(*[[[[['|_|','| |'][c=='0'],'|_ '][c in'56'],' _|'][c in'23'],'  |'][c in'17']for c in s],sep='')
+print(*[['|',' '][c in'1723']+['_',' '][c in'017']+['|',' '][c in'56']for c in s],sep='')
 print(*[[[['|_|',' _|'][c in'359'],'|_ '][c=='2'],'  |'][c in'147']for c in s],sep='')
