9ffe1cee85cc83cbfee7399a547f28851e45c853
Heathcorp <heath@manningf.com>
2022-11-23 12:22:28 +1100

    Reduce happynumbers.py to 102 bytes

--- Happy Numbers/happynumbers.py
+++ Happy Numbers/happynumbers.py
@@ -1,4 +1,4 @@
-def h(n,d=0):
- s=sum(map(lambda x:int(x)**2,list(str(n))))
- return h(s,d+1)if d<9else s==1
-[print(i)for i in range(201)if h(i)]
+for i in range(201):
+ k=i
+ for d in" "*9:k=sum(map(lambda x:int(x)**2,list(str(k))))
+ if k==1:print(i)
