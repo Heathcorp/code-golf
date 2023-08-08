b8e3ac2b73c4a72b9102cac0c4d2bb81d743a52d
Heathcorp <heath@manningf.com>
2023-08-08 19:01:07 +1000

    Update hexdump.c
    Reduce hexdump.c to 240 bytes

--- Hexdump/hexdump.c
+++ Hexdump/hexdump.c
@@ -1,18 +1,1 @@
-int main(int argc, char** argv){
-for(;--argc;){
-char* s = *++argv;
-int l = strlen(s);
- for(int i=0;i<l;i+=16,s+=16){
-  printf("%08x: ",i);
-  for(int j=0,k=0;j<16;j++,s[k]?k++:0){
-   printf(s[k]?"%02x":"  ",s[k]);
-   j&1?printf(" "):0;
-  }
-  putchar(' ');
-  for(int j=0;j<16&&s[j];j++)
-   putchar(s[j]-10?s[j]:'.');
-  puts("");
- }
-puts("");
-}
-}
+char*s;main(i,v,l,j,I)char**v;{for(;s=*++v;puts(""))for(l=i=j=I=0;l<strlen(s);puts("")){printf("%08x: ",l);l+=16;for(;i<l;i++%2?printf(" "):0)s[I]?printf("%02x",s[I++]):printf("  ");printf(" ");for(;j<l&&s[j];j++)putchar(s[j]-10?s[j]:46);}}
