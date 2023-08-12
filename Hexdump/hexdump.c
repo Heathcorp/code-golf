0b8479a2c63354634dc25051170dfd4d578e4da7
Heathcorp <heath@manningf.com>
2023-08-12 15:21:03 +1000

    Reduce hexdump.c to 232 bytes

--- Hexdump/hexdump.c
+++ Hexdump/hexdump.c
@@ -1,1 +1,1 @@
-char*s;main(i,v,l,j,I)char**v;{for(;s=*++v;puts(""))for(l=i=j=I=0;l<strlen(s);puts("")){printf("%08x:",l);for(l+=16;i<l;printf(s[I]?"%02x":"  ",s[I]?s[I++]:0))printf(" "+i++%2);for(printf("  ");j<l&&s[j];j++)putchar(s[j]-10?s[j]:46);}}
+main(i,v,l,j,I)char**v;{for(char*s,*t;s=t=*++v;puts(""))for(l=i=j=I=0;l<strlen(*v);puts("")){for(printf("%08x:",l),l+=16;i<l;printf(*s?"%02x":"  ",*s?*s++:0))printf(" "+i++%2);for(printf("  ");j<l&&*t;j++,t++)putchar(*t-10?*t:46);}}
