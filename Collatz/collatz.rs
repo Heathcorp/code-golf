2f919676ec54412e31a21b4642cb8f93914ae90a
Heathcorp <heath@manningf.com>
2023-08-26 11:13:07 +1000

    Reduce collatz.rs to 108 bytes

--- Collatz/collatz.rs
+++ Collatz/collatz.rs
@@ -1,8 +1,2 @@
-fn main(){
-for i in 1..1001{
-let mut n = 0;
-let mut j = i;
-while j>1{n+=1;if j%2>0{j=j*3+1}else{j/=2}}
-println!("{n}");
-}
-}
+fn main(){for i in 1..1001{let(mut j,mut n)=(i,0);while j>1{n+=1;j=if j%2>0{j*3+1}else{j/2}}print!("{n}
+")}}
