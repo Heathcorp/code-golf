2af9419c4b7df873a39adf940f18c6688c7cb43f
Heathcorp <heath@manningf.com>
2024-01-31 12:31:35 +1100

    Reduce fizzbuzz.clj to 115 bytes

--- Fizz Buzz/fizzbuzz.clj
+++ Fizz Buzz/fizzbuzz.clj
@@ -1,1 +1,1 @@
-(loop[i 1 j 99](println(str(if(=(mod i 3)0)"Fizz""")(if(=(mod i 5)0)"Buzz""")(if(>(*(mod i 3)(mod i 5))0)i"")))(if(> j 0)(recur(inc i)(dec j))0))
+(doseq[i(range 1 101)](let[a(=(mod i 3)0)b(=(mod i 5)0)](println(str(if a"Fizz""")(if b"Buzz""")(if(or a b)""i)))))
