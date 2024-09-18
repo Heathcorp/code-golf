f6845b55334725fa8ff6e607200e65a3d735afb4
Heath Manning <heath@manningf.com>
2024-09-18 18:28:51 +1000

    Reduce collatz.pl to 99 bytes.

--- Collatz/collatz.pl
+++ Collatz/collatz.pl
@@ -1,3 +1,2 @@
-c(N,D):-N=1,writeln(D);n(N,M),T is D+1,c(M,T).
-n(N,M):-M is N/2,integer(M),!;M is N*3+1.
+c(N,D):-N=:=1,C is D,writeln(C);mod(N,2)<1,!,c(N/2,D+1);c(N*3+1,D+1).
 :-between(1,1000,G),\+c(G,0).
