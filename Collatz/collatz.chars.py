7ae3e96d0c510210a3a9c4d191555c91e2f5d706
Flekay <71941122+Flekay@users.noreply.github.com>
2025-06-19 04:46:28 +0200

    Reduce collatz.py to 62 bytes

---
+++ Collatz/collatz.chars.py
@@ -0,0 +1,6 @@
+#for i in range(1,1001):
+# b=0
+# while i>1:i=[i//2,i*3+1][i%2];b+=1
+# print(b)
+# https://clemg.github.io/pythongolfer/
+exec(bytes('潦⁲⁩湩爠湡敧ㄨㄬ〰⤱਺戠〽 桷汩⁥㹩㨱㵩楛⼯ⰲ⩩⬳崱楛㈥㭝⭢ㄽ 牰湩⡴⥢','u16')[2:])
